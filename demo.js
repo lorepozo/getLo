644942c704276ecc41fced7f463049a51b10d2d9
Lucas Morales <lmorales@edx.org>
2014-10-05 10:07:13 -0400

    remove demo

--- demo.js
+++
@@ -1,8 +0,0 @@
-$.ajax({
-	url: 'https://api.parse.com/1/push',
-	type: "POST",
-	contentType: "application/json",
-	port: 443,
-	data: JSON.stringify({"where": {"objectId": 'pC2h2n3zkR'}, "data": {"alert": "lucasem wants to getLo"}}),
-	headers: {"X-Parse-Application-Id": 'm31OmA2VnCG1cR6DEzeBJzNOHPIkH3j0eAVPFR7P', "X-Parse-REST-API-Key": 'FxRbprMjTmLmoUchaLp3BxVxIDzZwCWFiEwhyoAT'}
-})
